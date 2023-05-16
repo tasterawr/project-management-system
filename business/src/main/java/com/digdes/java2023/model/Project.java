c6bb47c0772a6abd54be61f68b4e3df1a229ea2e
ilyataster <taster00@gmail.com>
2023-05-16 10:24:17 +0400

    Revert "Revert "fixes for PR review comments""
    This reverts commit a00c71f04f54a13e1269ffcb58f99a01ed8abb4f.

--- business/src/main/java/com/digdes/java2023/model/Project.java
+++ business/src/main/java/com/digdes/java2023/model/Project.java
@@ -1,5 +1,6 @@
 package com.digdes.java2023.model;
 
+import com.digdes.java2023.enums.ProjectStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- dto/src/main/java/com/digdes/java2023/dto/EmployeeDto.java
+++ dto/src/main/java/com/digdes/java2023/dto/EmployeeDto.java
@@ -1,18 +1,21 @@
 package com.digdes.java2023.dto;
 
+import com.digdes.java2023.enums.EmployeeStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeDto {
-    private String id;
+    private UUID id;
     private String lastName;
     private String firstName;
     private String surName;
     private String account;
     private String email;
-    private String status;
+    private EmployeeStatus status;
 }
